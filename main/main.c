6760897a8cd2c6224f5d7ed033573f2ca26e84d3
Jeija <norrepli@gmail.com>
2017-10-05 18:23:22 +0200

    Init dummy ap before spamming beacon frames
    Apparently using WIFI_MODE_NULL isn't sufficient if no WIFI configuration is already present in flash configuration memory.

--- main/main.c
+++ main/main.c
@@ -99,8 +99,26 @@ void app_main(void) {
 	ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
 	ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 	ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
-	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_NULL));
+
+	// Init dummy AP to specify a channel and get WiFi hardware into
+	// a mode where we can send the actual fake beacon frames.
+	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+	wifi_config_t ap_config = {
+		.ap = {
+			.ssid = "esp32-beaconspam",
+			.ssid_len = 0,
+			.password = "dummypassword",
+			.channel = 1,
+			.authmode = WIFI_AUTH_WPA2_PSK,
+			.ssid_hidden = 1,
+			.max_connection = 4,
+			.beacon_interval = 60000
+		}
+	};
+
+	ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &ap_config));
 	ESP_ERROR_CHECK(esp_wifi_start());
+	ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_NONE));
 
 	xTaskCreate(&spam_task, "spam_task", 2048, NULL, 5, NULL);
 }
